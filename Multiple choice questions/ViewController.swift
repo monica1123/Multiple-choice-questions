dcae5d8628469c258fc8ee44e1ee0a817fbad90f
monica1123 <shelly1123@hotmail.com>
2021-05-21 00:46:57 +0800

    let questionsDetail code more clear

--- Multiple choice questions/ViewController.swift
+++ Multiple choice questions/ViewController.swift
@@ -23,7 +23,26 @@ class ViewController: UIViewController {
     var score = 0
     var rightAnswer = ""
     
-    var questionsDetail = [Question(question: "誰還沒得過武漢肺炎", option: ["內野聖陽","阿部貞夫","長谷川博己","武田真治"], answer: "長谷川博己"), Question(question: "誰不曾是老菸槍", option: ["北川景子","深田恭子","蒼井優","榮倉奈奈"], answer: "深田恭子"), Question(question: "誰還不是最愛主持人Top1", option: ["有吉弘行","明石家秋刀魚","貴婦松子","塔摩利"], answer: "有吉弘行"), Question(question: "誰得過日本奧斯卡影帝", option: ["竹中直人","豐川悅司","柄本明","織田裕二"], answer: "柄本明"), Question(question: "誰當小三沒修成正果", option: ["岡田准一","篠原涼子","安達祐實","中谷美紀"], answer: "中谷美紀"), Question(question: "誰的另一半不是圈內人", option: ["速水茂虎道","西島秀俊","生田斗真","戶田惠梨香"], answer: "西島秀俊"), Question(question: "誰結婚證券交易所系統故障", option: ["堀北真希","新垣結衣","石原里美","福山雅治"], answer: "石原里美"), Question(question: "誰不曾因吸毒被捕", option: ["田口淳之介","山下智久","澤尻英龍華","伊勢谷友介"], answer: "山下智久"), Question(question: "誰不是歌舞伎演員", option: ["市川猿之助","野村萬齋","香川照之","松本幸四郎"], answer: "野村萬齋"), Question(question: "誰不是出身歌舞伎世家", option: ["中村七之助","市川海老藏","尾上松也","片岡愛之助"], answer: "片岡愛之助"), Question(question: "誰不是出身寶塚歌劇團", option: ["涼","黑木瞳","天海祐希","檀麗"], answer: "涼"), Question(question: "誰兄弟姊妹沒在圈內發展", option: ["永山絢斗","真榮田鄉敦","松田翔太","山崎弘也"], answer: "山崎弘也"), Question(question: "誰沒主持過紅白", option: ["相葉雅紀","二宮和也","大野智","櫻井翔"], answer: "大野智"), Question(question: "誰沒進過傑尼斯", option: ["反町隆史","Taka(one ok rock)","犬飼貴丈","本木雅弘"], answer: "犬飼貴丈"), Question(question: "誰還不是殿堂級國寶美男", option: ["新田真劍佑","平野紫耀","吉澤亮","山崎賢人"], answer: "新田真劍佑"), Question(question: "誰沒被寫入學校音樂教材", option: ["坂井泉水","安室奈美惠","初音未來","SMAP"], answer: "安室奈美惠"), Question(question: "誰不是同間經紀公司", option: ["福山雅治","仲里依紗","吉澤亮","橫濱流星"], answer: "橫濱流星"),Question(question: "誰沒演銀魂真人版電影", option: ["小栗旬","吉澤亮","柳樂優彌","佐藤健"], answer: "佐藤健"), Question(question: "誰不曾當過偶像", option: ["橋本環奈","大島優子","濱邊美波","白石麻衣"], answer: "濱邊美波"), Question(question: "誰和菅田將暉同個經紀人", option: ["杉野遙亮","松坂桃李","中村倫也","山崎賢人"], answer: "松坂桃李")]
+    var questionsDetail = [Question(question: "誰還沒得過武漢肺炎", option: ["內野聖陽","阿部貞夫","長谷川博己","武田真治"], answer: "長谷川博己"),
+                           Question(question: "誰不曾是老菸槍", option: ["北川景子","深田恭子","蒼井優","榮倉奈奈"], answer: "深田恭子"),
+                           Question(question: "誰還不是最愛主持人Top1", option: ["有吉弘行","明石家秋刀魚","貴婦松子","塔摩利"], answer: "有吉弘行"),
+                           Question(question: "誰得過日本奧斯卡影帝", option: ["竹中直人","豐川悅司","柄本明","織田裕二"], answer: "柄本明"),
+                           Question(question: "誰當小三沒修成正果", option: ["岡田准一","篠原涼子","安達祐實","中谷美紀"], answer: "中谷美紀"),
+                           Question(question: "誰的另一半不是圈內人", option: ["速水茂虎道","西島秀俊","生田斗真","戶田惠梨香"], answer: "西島秀俊"),
+                           Question(question: "誰結婚證券交易所系統故障", option: ["堀北真希","新垣結衣","石原里美","福山雅治"], answer: "石原里美"),
+                           Question(question: "誰不曾因吸毒被捕", option: ["田口淳之介","山下智久","澤尻英龍華","伊勢谷友介"], answer: "山下智久"),
+                           Question(question: "誰不是歌舞伎演員", option: ["市川猿之助","野村萬齋","香川照之","松本幸四郎"], answer: "野村萬齋"),
+                           Question(question: "誰不是出身歌舞伎世家", option: ["中村七之助","市川海老藏","尾上松也","片岡愛之助"], answer: "片岡愛之助"),
+                           Question(question: "誰不是出身寶塚歌劇團", option: ["涼","黑木瞳","天海祐希","檀麗"], answer: "涼"),
+                           Question(question: "誰兄弟姊妹沒在圈內發展", option: ["永山絢斗","真榮田鄉敦","松田翔太","山崎弘也"], answer: "山崎弘也"),
+                           Question(question: "誰沒主持過紅白", option: ["相葉雅紀","二宮和也","大野智","櫻井翔"], answer: "大野智"),
+                           Question(question: "誰沒進過傑尼斯", option: ["反町隆史","Taka(one ok rock)","犬飼貴丈","本木雅弘"], answer: "犬飼貴丈"),
+                           Question(question: "誰還不是殿堂級國寶美男", option: ["新田真劍佑","平野紫耀","吉澤亮","山崎賢人"], answer: "新田真劍佑"),
+                           Question(question: "誰沒被寫入學校音樂教材", option: ["坂井泉水","安室奈美惠","初音未來","SMAP"], answer: "安室奈美惠"),
+                           Question(question: "誰不是同間經紀公司", option: ["福山雅治","仲里依紗","吉澤亮","橫濱流星"], answer: "橫濱流星"),
+                           Question(question: "誰沒演銀魂真人版電影", option: ["小栗旬","吉澤亮","柳樂優彌","佐藤健"], answer: "佐藤健"),
+                           Question(question: "誰不曾當過偶像", option: ["橋本環奈","大島優子","濱邊美波","白石麻衣"], answer: "濱邊美波"),
+                           Question(question: "誰和菅田將暉同個經紀人", option: ["杉野遙亮","松坂桃李","中村倫也","山崎賢人"], answer: "松坂桃李")]
     
     func play() {
         numberLabel.text = "\(index + 1)"
