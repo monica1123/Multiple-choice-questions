90292aeb06497a053fe511239fcc6d9143eec15f
monica1123 <shelly1123@hotmail.com>
2021-05-21 00:17:57 +0800

    change cloud image

---
+++ Multiple choice questions/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Multiple choice questions
+//
+//  Created by Monica Lo on 2021/5/19.
+//
+
+import Foundation
+
+struct Question {
+    var question: String
+    var option: [String]
+    var answer: String
+}
